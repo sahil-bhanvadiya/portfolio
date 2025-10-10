2fc6d4d9ceb5a838146e143a34c8442cf30be56f
sahil-bhanvadiya <sahil.bhanvadiya.dev@gmail.com>
2025-10-10 23:04:46 +0530

    chore: update packages

--- animation/varients.ts
+++ animation/varients.ts
@@ -1,9 +1,11 @@
-export const formVariants = {
+import { Variants } from "framer-motion";
+
+export const formVariants: Variants = {
   hidden: { opacity: 0, y: 50 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
 };
 
-export const listVariants = {
+export const listVariants: Variants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
@@ -11,7 +13,7 @@ export const listVariants = {
   },
 };
 
-export const itemVariants = {
+export const itemVariants: Variants = {
   hidden: { opacity: 0, x: -20 },
   visible: { opacity: 1, x: 0, transition: { duration: 0.5, ease: "easeOut" } },
 };
