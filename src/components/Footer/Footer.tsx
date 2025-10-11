679ce5c78c28a839a49fc8e36f9fa1893a75fe14
sahil-bhanvadiya <sahil.bhanvadiya.dev@gmail.com>
2025-10-11 22:09:58 +0530

    feat: techs

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,13 @@
-import ThemeSwitcher from "../ui/ThemeSwitcher";
-
 export default function Footer() {
   return (
     <footer className="bg-darkest-gray border-t border-border-color flex flex-col items-center flex-nowrap gap-[10px] h-min justify-center overflow-visible py-10 px-0 relative w-full ">
       <div className="items-center flex flex-none flex-col flex-nowrap gap-[10px] h-min justify-center overflow-hidden p-0 relative w-full  ">
         <div className="w-[80%] flex flex-col-reverse sm:flex-row items-center sm:items-start gap-5 sm:gap-0 flex-nowrap flex-none h-min justify-center overflow-hidden p-0 relative ">
-          <div className="shrink-0 flex justify-start flex-1 relative break-words whitespace-pre-wrap h-auto ">
+          <div className="shrink-0 flex justify-center flex-1 relative break-words whitespace-pre-wrap h-auto ">
             <p className="text-lg text-light-gray-2 font-medium">
-              &copy; {new Date().getFullYear()} Parwez. All rights reserved.
+              &copy; {new Date().getFullYear()} Sahil Bhanvadiya. All rights
+              reserved.
             </p>
-          </div>
-
-          <div className="">
-            <ThemeSwitcher />
           </div>
         </div>
       </div>
