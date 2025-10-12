d59a97a73d6560a1c0f6e8059f928ae3efc7454c
sahil-bhanvadiya <sahil.bhanvadiya.dev@gmail.com>
2025-10-12 15:27:43 +0530

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  experimental: {
-    optimizeCss: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  compress: true,
 };
 
 export default nextConfig;
